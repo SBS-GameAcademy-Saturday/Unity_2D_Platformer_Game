523365d0560476aca3e6c051a14d343a664e468b
Uniun <jajazxcv@naver.com>
2024-07-27 17:45:20 +0900

    수업 자료 Import2

---
+++ Item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Item Data", menuName = "Scriptable Object/Item Data", order = 0)]
+public class Item : ScriptableObject
+{
+	public int ItemID;
+}
+
+[CreateAssetMenu(fileName = "Item Data", menuName = "Scriptable Object/Item Data", order = 0)]
+public class Item2 : ScriptableObject
+{
+	public int ItemID;
+	public int ItemType;
+	public bool Comsumable;
+}
+
